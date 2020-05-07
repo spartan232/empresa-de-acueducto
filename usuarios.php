11171005434963222863b6a20a5ac9f91fd4973e
aceros123 <57160341+aceros123@users.noreply.github.com>
2020-05-06 22:11:18 -0500

    usuarios
    creacion pg de usuarios (clientes)

---
+++ usuarios.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Usuario</title>
+    <link rel="icon" type="image/png" href="imagenes/logo.png" sizes="64x64">
+    <link rel="stylesheet" href="css/Style.css">
+</head>
+<body>
+    
+</body>
+</html>
